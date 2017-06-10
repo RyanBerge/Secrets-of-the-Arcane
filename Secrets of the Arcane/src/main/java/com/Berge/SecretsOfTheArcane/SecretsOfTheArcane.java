cfbb7651caad232c1c5911a83790909d5b5d9d16
RyanBerge <ryanberge13@gmail.com>
2017-06-09 23:06:32 -0700

    asd

--- Secrets of the Arcane/src/main/java/com/Berge/SecretsOfTheArcane/SecretsOfTheArcane.java
+++ Secrets of the Arcane/src/main/java/com/Berge/SecretsOfTheArcane/SecretsOfTheArcane.java
@@ -1,14 +1,14 @@
-package com.example.examplemod;
+package com.Berge.SecretsOfTheArcane;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = SecretsOfTheArcane.MODID, version = SecretsOfTheArcane.VERSION)
+public class SecretsOfTheArcane
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "secretsofthearcane";
     public static final String VERSION = "1.0";
     
     @EventHandler
